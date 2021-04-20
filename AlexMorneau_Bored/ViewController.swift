5e51d3c5a00dd6c373c77db00dd3d59085056bd5
Alex Morneau <alexmorneau1992@gmail.com>
2021-04-20 10:36:36 -0400

    Initial Commit

---
+++ AlexMorneau_Bored/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlexMorneau_Bored
+//
+//  Created by user191445 on 4/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
